637425f9433a6f16beb076e4f96290cae9868fdf
JoaquinParrilla <joaquin.p.olivera@gmail.com>
2020-06-25 16:53:56 -0300

    Inicio Unidad 7 Sobrecarga de operadores

--- Curso_Programacion_en_Kotlin/src/Unidad6/Clase10_Funciones_de_extension.kt
+++ Curso_Programacion_en_Kotlin/src/Unidad6/Clase10_Funciones_de_extension.kt
@@ -24,13 +24,69 @@ fun String.mitadSegunda(): String{
     return this.substring((this.length / 2)..(this.length - 1))
 }
 
+// Ejercicio 2:
+// Agregar fun de extension a IntArray que permita
+// imporimir todos sus elementos en una única linea
+fun IntArray.printOneLine(){
+    print("[")
+    for (i in 0..this.lastIndex){
+        if(i != this.lastIndex)
+            print("${this[i]}, ")
+        else
+            print("${this[i]}")
+    }
+    println("]")
+}
+
 fun main(args: Array<String>) {
     // Prueba:
-    val nombre: String = "Matias"
-    print("Palabra a partir en dos: $nombre")
-    println()
-    println("Primer mitad de la palabra: ${nombre.mitadPrimera()} ")
-    println("Segunda mitad de la palabra: ${nombre.mitadSegunda()}")
-    //println(nombre.mitadSegunda())
+//    val nombre: String = "Matias"
+//    print("Palabra a partir en dos: $nombre")
+//    println()
+//    println("Primer mitad de la palabra: ${nombre.mitadPrimera()} ")
+//    println("Segunda mitad de la palabra: ${nombre.mitadSegunda()}")
+
+    val unArray: IntArray = IntArray(5)
+    for(i in 0..4)
+        unArray[i] = i
+
+    unArray.printOneLine()
+    val word: String = "Pedro"
+    word.printChars()
+
+    println(); println()    //espacios en consola
+
+    val integer = 4
+    integer.printUntil(12)
+
 }
+
+// Ejercicio 3:
+// Agregar a la clase String un método imprimir que muestre todos los caracteres que
+// tiene almacenado en una línea.
+
+fun String.printChars(){
+    val charArray: CharArray = this.toCharArray()
+    print("[")
+    for (i in 0..this.lastIndex){
+        if(i != this.lastIndex)
+            print("${this[i]}, ")
+        else
+            print("${this[i]}")
+    }
+    println("]")
+}
+
+// Ejercicio 4:
+// Codicar la función de extensión llamada "hasta" que debe extender la clase Int y tiene
+// por objetivo mostrar desde el valor entero que almacena el objeto hasta el valor que
+// llega como parámetro:
+
+fun Int.printUntil(until: Int){
+    for (i in this..until){
+        println("$i")
+    }
+}
+
+
 
---
+++ Curso_Programacion_en_Kotlin/src/Unidad7/Clase1_Introduccion_sobrecarga_operadores.kt
@@ -0,0 +1,88 @@
+package Unidad7
+
+import Unidad6.mult
+
+/**
+ * Created by JoaquínParrilla on 25/06/2020.
+ * Clase 1: Sobrecarga de Operadores - Introducción
+ * Página del teórico: inicia en 206
+ * Temas:
+ *      Sobrecarga de Operadores
+ * <>
+El lenguaje Kotlin permite que ciertos operadores puedan sobrecargarse y actúen de
+diferentes maneras según al objeto que se aplique.
+La sobrecarga de operadores debe utilizarse siempre y cuando tenga sentido para la clase
+que se está implementando. Los conceptos matemáticos de vectores y matrices son casos
+donde la sobrecarga de operadores nos puede hacer nuestro código más legible y elegante.
+Para sobrecargar los operadores +, -, * y / debemos implementar una serie de métodos
+especiales dentro de la clase:  todo VER TEÓRICO
+Ejercicio 01:
+    Declarar una clase llamada Vector que administre un array de 5 elementos de tipo entero y
+    cargar valores aleatorios entre 1 y 10. Sobrecargar los operadores +, -, * y /
+    En la main definir una serie de objetos de la clase Vector y operar con ellos
+ **/
+
+class Vector {
+    val array: IntArray = IntArray(5)
+
+    fun cargarArray(){
+        for(i in array.indices)
+            array[i] = ((Math.random() * 11) + 1).toInt()
+    }
+
+    fun imprimir(){
+        for (elem in array)
+            print("$elem ")
+        println()
+    }
+
+    operator fun plus(vector: Vector): Vector {        // sobrecarga del operador +
+        var suma = Vector()
+        for(i in array.indices)
+            suma.array[i] = array[i] + vector.array[i]
+        return suma
+    }
+
+    operator fun minus(vector: Vector): Vector{
+        var resta = Vector()
+        for(i in array.indices)
+            resta.array[i] = array[i] - vector.array[i]
+        return resta
+    }
+
+    operator fun times(vector: Vector): Vector{
+        var mult = Vector()
+        for(i in array.indices)
+            mult.array[i] = array[i] * vector.array[i]
+        return mult
+    }
+
+    operator fun div(vector: Vector):Vector{
+        var divi = Vector()
+        for (i in array.indices)
+            divi.array[i] = array[i] / vector.array[i]
+        return  divi
+    }
+}
+
+fun main(args: Array<String>) {
+    val v1 = Vector()
+    v1.cargarArray()
+    v1.imprimir()
+    val v2 = Vector()
+    v2.cargarArray()
+    v2.imprimir()
+    val vSuma = v1 + v2
+    println("Suma componente a componente de los dos vectores")
+    vSuma.imprimir()
+    val vResta = v1 - v2
+    println("La resta componente a componente de los dos vectores")
+    vResta.imprimir()
+    val vProducto = v1 * v2
+    println("El producto componente a componente de los dos vectores")
+    vProducto.imprimir()
+    val vDivision = v1 / v2
+    println("La división componente a componente de los dos vectores")
+    vDivision.imprimir()
+
+}
